b0b0bd0600da890751161e6cd45b3d314a8c9ec0
Hongjin Cao <caobeef@MacBook-Pro-12.local>
2023-07-11 20:27:29 -0400

    i added a new file 5

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-hrs = input("Enter a number of hours: ")
-int(hrs) *= 60
-print(hrs)
+hrs = int(input("Enter a number of hours: "))
+hrs *= 60
+print("Minutes: " + hrs)
 # a program that asks the user for a number of hours and prints out the same duration in minutes
