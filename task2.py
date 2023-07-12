9bdb8d7676614e3219a5b630a4c201d2083e27fb
Hongjin Cao <caobeef@MacBook-Pro-12.local>
2023-07-11 20:14:55 -0400

    i added a new file 3

---
+++ task2.py
@@ -0,0 +1,10 @@
+choice = input("hours to minutes or minutes to hours")
+if choice == hours to minutes:
+    hours = input("Enter number of hours")
+    hours *= 60
+    print hours
+elif choice == minutes to hours:
+    mins = input("Enter number of minutes")
+    mins /= 60
+    print mins
+# a program that converts from hours to minutes or minutes to hours depending on the user's choice.
---
+++ task3.py
@@ -0,0 +1,4 @@
+word = input("Enter a word: ")
+num = len(word)
+print(num)
+# a program that lets the user type a word and prints how many letters are in that word.
